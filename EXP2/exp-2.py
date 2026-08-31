16ee89bff66f6c2cf6f2462b84c7551c11bb27d7
Nivya <srinivya02@gmail.com>
2026-08-31 09:52:29 +0530

    Delete EXP2 directory

--- EXP2/exp-2.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-df = pd.read_csv("job_history.csv")
-job_counts = df['EMPLOYEE_ID'].value_counts()
-multiple_jobs = job_counts[job_counts >= 2].index
-print(df[df['EMPLOYEE_ID'].isin(multiple_jobs)])
